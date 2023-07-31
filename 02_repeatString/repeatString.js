d195192fb140bfbb15fcc45995782410e66f9102
Luan Nguyen <tailuan30092000@gmail.com>
2023-07-31 14:54:47 -0700

    Added js exercise, completed 1st, 2nd, and 3rd exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(content, n) {
+    if (n < 0) {
+        return "ERROR";
+    }
+    if (n == 0) {
+        return "";
+    }
+    let result = content;
+    for (let i = 0; i < n - 1; i++) {
+        result += content;
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
-};
+const reverseString = function(content) {
+    return content.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = reverseString;
