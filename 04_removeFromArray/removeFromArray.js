a331ba5e0cfab31bab9ccd09714d36970541c75b
Luan Nguyen <tailuan30092000@gmail.com>
2023-07-31 15:24:50 -0700

    Completed exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray1 = function(array, ...args ) {
+    return array.filter((element) => (!args.includes(element)));
 };
+
+const removeFromArray = function(array, ...args) {
+    for (let i = 0; i < args.length; i++) {
+        let index = array.indexOf(args.at(i));
+        if (index != -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
